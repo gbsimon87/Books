9e26317dc05b240f7e14bbc26c17f9959233553d
Simon Cordova <y.simon.cordova@gmail.com>
2019-08-23 13:07:28 +0100

    Connect to mongoDB database

--- server/app.js
+++ server/app.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
 const schema = require("./schema");
+const mongoose = require("mongoose");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+mongoose.connect(
+  "mongodb+srv://gbsimon87:gbsimon87@cluster0-kvyxy.mongodb.net/test?retryWrites=true&w=majority"
+);
+mongoose.connection.once("open", () => {
+  console.log("connected to database");
+});
 
 app.use(
   "/graphql",
